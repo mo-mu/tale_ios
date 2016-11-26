db959d830427b60a5e2b202cb944c344c7c1ebd9
knulps <araspitm@gmail.com>
2016-11-26 15:40:20 +0900

    1. 페이지 몇 가지 추가. 2. navigation 추가.

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,17 @@ var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
 
 var styles = StyleSheet.create({
+	settingsBox: {
+		marginTop:100,
+		padding:10,
+		width:width-40,
+		backgroundColor:'transparent',
+		alignSelf:'center',
+	},
+	settingsText: {
+		fontSize: 16,
+		color:'white'
+	},
 	row: {
 		flexDirection: 'column',
 		padding: 10,
@@ -108,6 +119,13 @@ var styles = StyleSheet.create({
 	}
 });
 
+var NoTransition = {
+	opacity: {
+		value: 1.0,
+		type: 'constant',
+	}
+};
+
 /* eslint no-bitwise: 0 */
 var hashCode = function(str) {
 	var hash = 15;
@@ -138,6 +156,14 @@ var MOCKED_QUESTIONS_DATA = [
 {question : '당신에게 가장 기억에 남는 여행지는 어디인가요?'},
 ];
 
+var MOCKED_DATE_DATA = [
+{date: '2016/ 10/ 12'},
+{date: '2016/ 11/ 01'},
+{date: '2016/ 11/ 10'},
+{date: '2016/ 11/ 18'},
+{date: '2016/ 11/ 20'},
+]
+
 var NavigationBarRouteMapper = {
 	Title(route, navigator, index, navState) {
         // return <Text style={ styles.title }>MY APP TITLE</Text>
@@ -165,12 +191,23 @@ var NavigationBarRouteMapper = {
     		return (
     			<TouchableHighlight style={{backgroundColor:'transparent'}}
     			underlayColor="transparent"
-    			onPress={() => { if (index > 0) { navigator.pop() } }}>
-    			<Image
-    			style={{overflow:'visible', width:30, height:30, marginRight:18, marginTop:5}}
-    			source={require('./img/check_btn.png')}/>
-    			</TouchableHighlight>
-    			)}
+    			onPress={() => {
+    				if (index > 0) {
+    					navigator.replace({
+    						component: AnswerText,
+    						passProps: {
+    							id: 'AnswerList',
+    							title: '제목',
+    							main: '내용',
+    						},
+    						onPress: this.onPress,
+    						rightText: 'ALERT!',
+    					})}}}>
+    					<Image
+    					style={{overflow:'visible', width:30, height:30, marginRight:18, marginTop:5}}
+    					source={require('./img/check_btn.png')}/>
+    					</TouchableHighlight>
+    					)}
     		else if(index == 0) {
     			return (
     				<TouchableHighlight style={{backgroundColor:'transparent'}}
@@ -187,8 +224,15 @@ var NavigationBarRouteMapper = {
     						onPress: this.onPress,
     						rightText: 'ALERT!'
     					})}},
-    					{text: '설정', onPress: () => console.log('Bar Pressed!')},
-    					{text: '취소', onPress: () => console.log('Bar Pressed!')}
+    					{text: '설정', onPress: () => navigator.push({
+    						component: Settings,
+    						passProps: {
+    							id: 'AnswerList',
+    						},
+    						onPress: this.onPress,
+    						rightText: 'ALERT!'
+    					})},
+    					{text: '취소', onPress: () => console.log('Cancel Pressed!')}
     					]
     					)}>
     				<Image
@@ -240,120 +284,213 @@ var NavigationBarRouteMapper = {
     	}
     }
 
+     //미리 작성한 답변 보기 클래스
+     class AnswerText extends Component {
+     	constructor(props) {
+     		super(props);
+     		this.state = {
+     			text: '',
+     		};
+     		this._onPress = this._onPress.bind(this);
+     	}
+     	writeProcess() {
+     		console.log("writeProcess 진입");
+     	}
 
-    //질문 클래스
-    class Question extends Component {
-    	constructor(props) {
-    		super(props);
-    		this.state = {
-    			// movies: null
-    			question: MOCKED_QUESTIONS_DATA[0].question,
-    			position : 0
-    		};
-    		this.gotoNext = this.gotoNext.bind(this)
-    		this._onPressButton = this._onPressButton.bind(this)
-    	}
+     	render() {
+     		return (
+     			<View style ={styles.imageContainer}>
+     			<Image
+     			style={styles.backgroundImage}
+     			source={require('./img/main_bg_img.png')}>
+     			<TouchableHighlight onPress={this._onPress}>
+     			<View>
+     			<View style={styles.answerHeader}>
+     			<Text style={styles.answerHeaderText}>당신에게 가장 기억에 어쩌고저쩌고</Text>
+     			</View>
+     			<View style={styles.answerEditBox}>
+     			<Text
+     			placeholder="* 이 곳에 당신의 이야기를 적어보세요.`"
+     			style={styles.multiline}
+     			value={this.state.text}
+     			/>
+     			</View>
+     			</View>
+     			</TouchableHighlight>
+     			</Image>
+     			</View>
+     			);
+     	}
 
-    	render() {
+     	_onPress(rowID: number) {
+     		this.props.navigator.push({
+     			component: AnswerEdit,
+     			passProps: {
+     				id: 'AnswerList',
+     				title: '제목',
+     				main: '내용',
+     			},
+     			onPress: this.onPress,
+     			rightText: 'ALERT!'
+     		});
+     	}
+     }
 
-    		var movie = MOCKED_QUESTIONS_DATA[this.position];
-    		return (
-    			<View style ={styles.imageContainer}>
-    			<Image
-    			style={styles.backgroundImage}
-    			source={require('./img/main_bg_img.png')}>
+  //설정 클래스
+  class Settings extends Component {
+  	constructor(props) {
+  		super(props);
+  		this.state = {
+  			text: '',
+  		};
+  		this._onPress = this._onPress.bind(this);
+  	}
+  	writeProcess() {
+  		console.log("writeProcess 진입");
+  	}
 
-    			<View
-    			style={{marginLeft: 35, marginTop: 120}}>
-    			<Image
-    			style={{overflow: 'visible',  width: 40, height: 40}}
-    			source={require('./img/small_fox.png')}/>
-    			</View >
+  	render() {
+  		return (
+  			<View style ={styles.imageContainer}>
+  			<Image
+  			style={styles.backgroundImage}
+  			source={require('./img/main_bg_img.png')}>
+  			<View style={styles.settingsBox}>
+  			<Text style={styles.settingsText}>만든이 : MOMU</Text>
+  			</View>
+  			</Image>
+  			</View>
+  			);
+  	}
 
-    			<TouchableHighlight onPress={this.gotoNext} style={styles.textBox}>
-    			<Text style={{backgroundColor: 'white'}}>{this.state.question}</Text>
-    			</TouchableHighlight>
-
-
-    			<TouchableHighlight onPress={this._onPressButton} style={{alignSelf:'center', marginTop:150}}>
-    			<Text style={{color:'white',textDecorationLine: 'underline', }}>다른 질문 보여줘!</Text>
-    			</TouchableHighlight>
-
-    			</Image>
-    			</View>
-    			);
-    	}
-
-    	//질문 박스 클릭 이벤트
-    	_onPressQuestion() {
-    		this.setState({
-    			question: this.state.question,
-    			position : this.state.position,
-    		});
-    		console.log("박스 클릭함");
-    	}
-
-    	gotoNext() {
-    		this.props.navigator.push({
-    			component: AnswerEdit,
-    			passProps: {
-    				id: 'AnswerEdit',
-    			},
-    			onPress: this.onPress,
-    			rightText: 'ALERT!'
-    		})
-    	}
-
-
-    	//다른 질문 보여줘 버튼 클릭 이벤트
-    	_onPressButton() {
-    		pos = this.state.position;
-    		if(pos == MOCKED_QUESTIONS_DATA.length-1) {
-    			pos = 0;
-    		} else {
-    			pos++;
-    		}
-
-    		this.setState({
-    			question: MOCKED_QUESTIONS_DATA[pos].question,
-    			position : pos
-    		});
-    	}
-    }
+  	_onPress(rowID: number) {
+  		this.props.navigator.push({
+  			component: AnswerEdit,
+  			passProps: {
+  				id: 'AnswerList',
+  				title: '제목',
+  				main: '내용',
+  			},
+  			onPress: this.onPress,
+  			rightText: 'ALERT!'
+  		});
+  	}
+  }
 
 
-    class AnswerList extends Component {
-    	constructor(props) {
-    		super(props);
 
-    		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    		this.state = {
-    			dataSource: ds.cloneWithRows(['글1', '글2','글3','글4']),
-    		};
+     //질문 클래스
+     class Question extends Component {
+     	constructor(props) {
+     		super(props);
+     		this.state = {
+     			question: MOCKED_QUESTIONS_DATA[0].question,
+     			position : 0
+     		};
+     		this.gotoNext = this.gotoNext.bind(this)
+     		this._onPressButton = this._onPressButton.bind(this)
+     	}
 
-    		this._renderRow = this._renderRow.bind(this);
-    	}
+     	render() {
 
-    	render() {
-    		var movie = MOCKED_QUESTIONS_DATA[this.position];
-    		return (
-    			<View style ={styles.imageContainer}>
-    			<Image
-    			style={styles.backgroundImage}
-    			source={require('./img/main_bg_img.png')}>
-    			<View style={{flex: 1, paddingTop: 80, paddingLeft:18}}>
-    			<ListView
-    			dataSource={this.state.dataSource}
-    			renderRow = {this._renderRow}
-    			/>
-    			</View>
-    			</Image>
-    			</View>
-    			);
-    	}
+     		var movie = MOCKED_QUESTIONS_DATA[this.position];
+     		return (
+     			<View style ={styles.imageContainer}>
+     			<Image
+     			style={styles.backgroundImage}
+     			source={require('./img/main_bg_img.png')}>
 
-    	_renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
-    		var rowHash = Math.abs(hashCode(rowData));
+     			<View
+     			style={{marginLeft: 35, marginTop: 120}}>
+     			<Image
+     			style={{overflow: 'visible',  width: 40, height: 40}}
+     			source={require('./img/small_fox.png')}/>
+     			</View >
+
+     			<TouchableHighlight onPress={this.gotoNext} style={styles.textBox}>
+     			<Text style={{backgroundColor: 'white'}}>{this.state.question}</Text>
+     			</TouchableHighlight>
+
+
+     			<TouchableHighlight onPress={this._onPressButton} style={{alignSelf:'center', marginTop:150}}>
+     			<Text style={{color:'white',textDecorationLine: 'underline', }}>다른 질문 보여줘!</Text>
+     			</TouchableHighlight>
+
+     			</Image>
+     			</View>
+     			);
+     	}
+
+     	//질문 박스 클릭 이벤트
+     	_onPressQuestion() {
+     		this.setState({
+     			question: this.state.question,
+     			position : this.state.position,
+     		});
+     		console.log("박스 클릭함");
+     	}
+
+     	gotoNext() {
+     		this.props.navigator.push({
+     			component: AnswerEdit,
+     			passProps: {
+     				id: 'AnswerEdit',
+     			},
+     			onPress: this.onPress,
+     			rightText: 'ALERT!'
+     		})
+     	}
+
+
+     	//다른 질문 보여줘 버튼 클릭 이벤트
+     	_onPressButton() {
+     		pos = this.state.position;
+     		if(pos == MOCKED_QUESTIONS_DATA.length-1) {
+     			pos = 0;
+     		} else {
+     			pos++;
+     		}
+
+     		this.setState({
+     			question: MOCKED_QUESTIONS_DATA[pos].question,
+     			position : pos
+     		});
+     	}
+     }
+
+
+     class AnswerList extends Component {
+     	constructor(props) {
+     		super(props);
+
+     		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+     		this.state = {
+     			dataSource: ds.cloneWithRows(['글1', '글2','글3','글4','글5']),
+     		};
+
+     		this._renderRow = this._renderRow.bind(this);
+     	}
+
+     	render() {
+     		var movie = MOCKED_QUESTIONS_DATA[this.position];
+     		return (
+     			<View style ={styles.imageContainer}>
+     			<Image
+     			style={styles.backgroundImage}
+     			source={require('./img/main_bg_img.png')}>
+     			<View style={{flex: 1, paddingTop: 80, paddingLeft:18}}>
+     			<ListView
+     			dataSource={this.state.dataSource}
+     			renderRow = {this._renderRow}
+     			/>
+     			</View>
+     			</Image>
+     			</View>
+     			);
+     	}
+
+     	_renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
+     		var rowHash = Math.abs(hashCode(rowData));
     		// var imgSource = THUMB_URLS[rowHash % THUMB_URLS.length];
     		var imgSource = require('./img/small_fox.png')
     		return (
@@ -365,14 +502,14 @@ var NavigationBarRouteMapper = {
     			<View style={styles.row}>
     			<View style={{flexDirection:'row'}}>
     			<Text style={styles.rowTextTop}>
-    			2016/ 11/ 22
+    			{MOCKED_DATE_DATA[rowID].date}
     			</Text>
     			<Image
     			style={{overflow: 'visible',  width: 10, height: 10, marginLeft:5, marginTop:2}}
     			source={require('./img/small_fox.png')}/>
     			</View>
     			<Text style={styles.rowTextMain}>
-    			당신에게 가장 좋았던 기억은 무엇인가요?
+    			{MOCKED_QUESTIONS_DATA[rowID].question}
     			</Text>
     			</View>
     			</View>
@@ -382,7 +519,7 @@ var NavigationBarRouteMapper = {
 
     	_pressRow(rowID: number) {
     		this.props.navigator.push({
-    			component: AnswerEdit,
+    			component: AnswerText,
     			passProps: {
     				id: 'AnswerList',
     			},
